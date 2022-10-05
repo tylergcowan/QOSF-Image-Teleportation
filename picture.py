c981d54c6a3e2c5a4f7bcd677e25100cce5f1715
tylergcowan <113568149+tylergcowan@users.noreply.github.com>
2022-10-05 13:04:08 -0400

    picture updates

--- picture.py
+++ picture.py
@@ -1,7 +1,24 @@
-with open("cropped_qosf.jpg", "rb") as image:
-  f = image.read()
-  b = bytearray(f)
-  for i in b:
-        
-    print (format(b[i],'08b'))
+with open("trythis - Copy.jpg", "rb") as image:
+    f = image.read()
+    b = bytearray(f)
+    print(len(b))
+    bi=[]
+    j=0
+    for i in b:
+        j=j+1
+        bi.append(format(b[i],'08b'))
 
+
+
+
+
+
+
+exit()
+
+# split this into groups of 4, then send
+i+=1
+process=[]
+for val in bi:
+    pass
+    #print (val)
